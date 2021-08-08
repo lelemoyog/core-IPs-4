22014b1702a8294ec905e98eb200ca5cd809527a
saitemu_issa <saitemu.issa@student.moringaschool.com>
2021-08-08 15:04:34 +0300

    added routes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,24 @@
+import models.Hero;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
+
 public class App {
     public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Hero> heroes = Hero.getAll();
+            model.put("heroes", heroes);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
 
     }
 }
