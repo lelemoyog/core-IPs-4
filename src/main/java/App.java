5f6159f2af34b8d8a1f8ef21ca425eb8b39810eb
saitemu_issa <saitemu.issa@student.moringaschool.com>
2021-08-09 17:58:19 +0300

    added routing for hero

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,8 +6,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.get;
-import static spark.Spark.staticFileLocation;
+import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
@@ -19,6 +18,26 @@ public class App {
             model.put("heroes", heroes);
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
-
-    }
+        post("/heroes/new", (request, response) ->{
+            Map<String, Object> model = new HashMap<>();
+            String name = request.queryParams("hero");
+            String power = request.queryParams("power");
+            String weakness= request.queryParams("weakness");
+            int age = Integer.parseInt(request.queryParams("age"));
+            Hero newHero = new Hero(name,age,power,weakness);
+            model.put("hero", newHero);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/heroes/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "form.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/heroes/:id", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfHeroToFind = Integer.parseInt(req.params(":id")); //pull id - must match route segment
+            Hero foundHero = Hero.findById( idOfHeroToFind); //use it to find post
+            model.put("hero", foundHero); //add it to model for template to display
+            return new ModelAndView(model, "hero-detail.hbs"); //individual post page.
+        }, new HandlebarsTemplateEngine());
+}
 }
